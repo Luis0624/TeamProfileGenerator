2090f1ea4211f75a16e44b14045aeb0b83126272
Luis0624 <luis.apgo12@gmail.com>
2021-06-30 14:28:49 -0400

    created general switch case statement for the 1st inquierer prompt

--- Assets/server.js
+++ Assets/server.js
@@ -14,3 +14,42 @@ const connection = mysql.createConnection({
     if (err) throw err;
     init();
   });
+
+  const addItem = () => {
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'rawlist',
+        message: 'Please select an item to add:',
+        choices: [
+          'Department',
+          'Role',
+          'Employee',
+          'Main menu',
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+            case 'Department':
+                addDepartment();
+            break;
+      
+            case 'Role':
+                addRole();
+            break;
+      
+            case 'Employee':
+                addEmployee();
+            break;
+      
+            case 'Main menu':
+                init();
+            break;
+      
+            default:
+                console.log('Not a valid option.');
+            break;  
+        }
+
+    });
+};
